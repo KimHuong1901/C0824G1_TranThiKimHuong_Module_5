b7aa7a141067819d0cbaac1538ff8f4c737dfd42
KimHuong1901 <trankimhuong1901@gmail.com>
2025-01-17 15:53:37 +0700

    bai_tap ES6

--- ss1/bai_1/bai1.js
+++ ss1/bai_1/bai1.js
@@ -1,14 +1,11 @@
 //Hàm kiểm tra số nguyên tố
-let checkNum =(number)=> {
+const isPrime = (number) => {
     if (number <= 1) return false;
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-        if (number % i === 0) {
-            return false;
-        }
-    }
-    return true;
+    return Array.from({ length: Math.floor(Math.sqrt(number)) - 1 }, (_, i) => i + 2)
+        .every(divisor => number % divisor !== 0);
 };
+
 //Lọc ra các số là số nguyên tố trong mảng số nguyên dùng filter
-const arrNum = [1, 2, 3, 4, 5, 6, 7, 8];
-const checkNumber = arrNum.filter(checkNum);
+const arrNum = [1, 2, 3, 4, 5, 6, 7, 8, 13, 11, 90];
+const checkNumber = arrNum.filter(isPrime);
 console.log(checkNumber);
