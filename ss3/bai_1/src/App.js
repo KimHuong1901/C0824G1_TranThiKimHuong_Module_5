a0148d9ad583f75518a54f2d57645cd81d5fd330
KimHuong1901 <trankimhuong1901@gmail.com>
2025-01-22 11:10:51 +0700

    update

--- ss3/bai_1/src/App.js
+++ ss3/bai_1/src/App.js
@@ -1,47 +1,10 @@
-import {Component} from "react";
+import Studentist from "./StudentList";
 
-class Studentist extends Component {
-    componentDidMount() {
-    }
-
-    constructor() {
-        super();
-        this.state = {
-            students: [
-                {
-                    id: 1,
-                    name: 'Nguyễn Văn A',
-                    age: 30,
-                    address: 'Hà Nội'
-                }
-            ]
-        }
-    }
-
-    render() {
-        return (
-            <>
-                <h2>Student List</h2>
-                <table border="1" className="table table-striped">
-                    <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Age</th>
-                        <th>Address</th>
-                    </tr>
-                    </thead>
-                    {this.state.students.map((student, index) => (
-                        <tr key={student.id}>
-                            <td>{student.id}</td>
-                            <td>{student.name}</td>
-                            <td>{student.age}</td>
-                            <td>{student.address}</td>
-                        </tr>
-                    ))}
-                </table>
-            </>
-        )
-    }
+function App() {
+    return (
+        <Studentist/>
+    );
 }
-export default Studentist;
+
+export default App;
+
--- ss3/bai_2/src/App.js
+++ ss3/bai_2/src/App.js
@@ -1,48 +1,9 @@
-import {Component} from "react";
-import "./App.css"
-class ToDoList extends Component{
-  componentDidMount(){
-  }
-  constructor(props) {
-    super(props);
-    this.state={
-      list: [],
-      item: ''
-    }
-  }
-  handleChange = (event) =>{
-    this.setState({
-      item: event.target.value
-    })
-  }
-  handleAddItem = () => {
-    const {item, list } = this.state;
-    if(item.trim() !== ''){
-      this.setState({
-        list: [...this.state.list, item],
-        item: ''
-      })
-    }
-  }
-  render(){
-    return (
-        <div className="container">
-          <div className="todo">
-            <h2> Todo List </h2>
-            <input
-                type="text"
-                value={this.state.item}
-                onChange={this.handleChange}
-            />
-            <button onClick={this.handleAddItem}>Add</button>
-            <ol>
-              {this.state.list.map((content, index) => (
-                  <li key={index}>{content}</li>
-              ))}
-            </ol>
-          </div>
-        </div>
-    )
-  }
+import ToDoList from "./ToDoList";
+
+function App() {
+  return (
+      <ToDoList/>
+  );
 }
-export default ToDoList;
+
+export default App;
